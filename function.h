b056a22a9ab981c9286497e57406223f0ed8ddca
qxlydo <135758752+qxlydo@users.noreply.github.com>
2025-01-22 00:08:39 +0500

    Add files via upload

--- function.h
+++ function.h
@@ -1,3 +1,4 @@
+
 #ifndef FUNCTION_H
 #define FUNCTION_H
 
@@ -5,28 +6,39 @@
 class Temperature{
 public:
 	float farengeitInCelsia(int farengeit) {
-        return (farengeit - 32) * 5.0 / 9.0; 
+        return (farengeit - 32) * 5.0 / 9.0;
     }
 
     float celsiaInFarengeit(int celsia) {
-        return (celsia * 9.0 / 5.0) + 32; 
+        return (celsia * 9.0 / 5.0) + 32;
     }
 
     float kelvinInCelsia(int kelvin) {
-        return kelvin - 273.15; 
+        return kelvin - 273.15;
     }
 
     float celsiaInKelvin(int celsia) {
-        return celsia + 273.15; 
+        return celsia + 273.15;
     }
 
     float kelvinInFarengeit(int kelvin) {
-        return (kelvin - 273.15) * 9.0 / 5.0 + 32; 
+        return (kelvin - 273.15) * 9.0 / 5.0 + 32;
     }
 
     float farengeitInKelvin(int farengeit) {
-        return (farengeit - 32) * 5.0 / 9.0 + 273.15; 
+        return (farengeit - 32) * 5.0 / 9.0 + 273.15;
     }
 };
+
+
+// index = massBody / ((height / 100) * (height / 100));
+
+class BMI{
+public:
+	float bodyMassIndex(float massa, float height){
+		return massa / ((height / 100) * (height / 100));
+	}
+};
+
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "function.h"
+#include <cmath>
 
 
 void converter_temperature(){
@@ -9,50 +10,83 @@ void converter_temperature(){
 	Temperature temp;
 
 	int valueTemp;
-	    std::cout << "Выберите как будем конвертировать: \n";
-	    std::cout << "1. Цельсии в Фаренгейт\n";
-	    std::cout << "2. Фаренгейт в Цельсии\n";
-	    std::cout << "3. Цельсии в Кельвины\n";
-	    std::cout << "4. Кельвины в Цельсии\n";
-	    std::cout << "5. Фаренгейты в Кельвины\n";
-	    std::cout << "6. Кельвины в Фаренгейты\n";
-
+    std::cout << "Выберите как будем конвертировать: \n";
+    std::cout << "1. Цельсии в Фаренгейт\n";
+    std::cout << "2. Фаренгейт в Цельсии\n";
+    std::cout << "3. Цельсии в Кельвины\n";
+    std::cout << "4. Кельвины в Цельсии\n";
+    std::cout << "5. Фаренгейты в Кельвины\n";
+    std::cout << "6. Кельвины в Фаренгейты\n";
+    std::cout << std::endl;   
+    std::cout << "Ввод: ";
     std::cin >> valueTemp;
     switch(valueTemp){
     	case 1:
+            std::cout << "Температура: ";
     		std::cin >> celsiaValue;
-    		std::cout << temp.celsiaInFarengeit(celsiaValue) << std::endl;
+    		std::cout << "Результат: " << temp.celsiaInFarengeit(celsiaValue) << std::endl;
     		break;
     	case 2:
+            std::cout << "Температура: ";
 			std::cin >> farengeitValue;
-			std::cout << temp.farengeitInCelsia(farengeitValue) << std::endl;
+			std::cout << "Результат: " << temp.farengeitInCelsia(farengeitValue) << std::endl;
     		break;
     	case 3:
+            std::cout << "Температура: ";
 			std::cin >> celsiaValue;
-			std::cout << temp.celsiaInKelvin(celsiaValue) << std::endl;
+			std::cout << "Результат: " << temp.celsiaInKelvin(celsiaValue) << std::endl;
     		break;
     	case 4:
+            std::cout << "Температура: ";
 			std::cin >> kelvinValue;
-			std::cout << temp.kelvinInCelsia(kelvinValue) << std::endl;
+			std::cout << "Результат: " << temp.kelvinInCelsia(kelvinValue) << std::endl;
     		break;
     	case 5:
+            std::cout << "Температура: ";
 			std::cin >> farengeitValue;
-			std::cout << temp.farengeitInKelvin(farengeitValue) << std::endl;
+			std::cout << "Результат: " << temp.farengeitInKelvin(farengeitValue) << std::endl;
     		break;
     	case 6:
+            std::cout << "Температура: ";
 			std::cin >> kelvinValue;
-			std::cout << temp.kelvinInFarengeit(kelvinValue) << std::endl;
+			std::cout << "Результат: " << temp.kelvinInFarengeit(kelvinValue) << std::endl;
     		break;
    		default:
    			std::cout << "Error";
-   			break;		
-	    }
-	}
+   			break;
+    }
+}
 
+void converterBmi(){
+    float massaValue, heightValue;
+    std::cout << "Введите рост(см): \n";
+    std::cin >> heightValue;
+    std::cout << "Введите вес: \n";
+    std::cin >> massaValue;
 
+    BMI bmi;
+    std::cout << "Результат: " << round(bmi.bodyMassIndex(massaValue, heightValue)) << std::endl;
+
+}
 void menu(){
-	std::cout << "Вы выбрали конвертер температур\n";
-	converter_temperature();
+    int choice;
+    std::cout << "Выберите конвертер: \n";
+    std::cout << "1. Индекс массы тела\n";
+    std::cout << "2. Конвертер температур\n";
+    std::cin >> choice;
+    switch(choice){
+        case 1:
+            std::cout << "Вы выбрали Индекс массы тела\n";
+            converterBmi();
+            break;
+        case 2:
+            std::cout << "Вы выбрали конвертер температур\n";
+            converter_temperature();
+            break;
+        default:
+            std::cout << "Error";
+   			break;
+    }
 }
 
 
